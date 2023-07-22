8f3ef337f294b7836e6073d3cc6af1d383a5a2ad
wen <zwen_020@163.com>
2023-07-22 18:42:02 +0800

    fix

--- app/models.py
+++ app/models.py
@@ -4,5 +4,5 @@ class Teacher(db.Model):
   __tablename__ = 'teachers'
 
   teacher_id = db.Column('teacher_id', db.String, primary_key=True, doc='teacher_id')
-  teacher_name = db.Column(db.String, doc='teacher_name')
-  password = db.Column(db.String, doc='password')
+  teacher_name = db.Column('teacher_name',db.String, doc='teacher_name')
+  password = db.Column('password',db.String, doc='password')

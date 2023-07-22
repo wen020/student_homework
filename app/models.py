2f9e31e2bca439c4f4463ecf8fa2f81c71be1819
wen <zwen_020@163.com>
2023-07-22 18:45:01 +0800

    fix

--- app/models.py
+++ app/models.py
@@ -3,6 +3,6 @@ from . import db
 class Teacher(db.Model):
   __tablename__ = 'teachers'
 
-  id = db.Column('id', db.String, primary_key=True, doc='id')
-  teacher_name = db.Column('teacher_name',db.String, doc='teacher_name')
-  password = db.Column('password',db.String, doc='password')
+  id = db.Column('id', db.String(64), primary_key=True, doc='id')
+  teacher_name = db.Column('teacher_name',db.String(64), doc='teacher_name')
+  password = db.Column('password',db.String(64), doc='password')

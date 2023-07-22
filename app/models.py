8c607e2b2151a91fbed61dc72553629d99eb6e61
wen <zwen_020@163.com>
2023-07-22 20:27:06 +0800

    fix

--- app/models.py
+++ app/models.py
@@ -1,14 +1,27 @@
 from . import db
+import uuid
+import enum
+from sqlalchemy import Enum
 
 
-class Teacher(db.Model):
-    __tablename__ = 'teachers'
+class TagEnum(enum.Enum):
+    student = 1
+    teacher = 2
 
-    id = db.Column('id', db.String(64), primary_key=True, doc='id')
-    teacher_name = db.Column('teacher_name', db.String(64), doc='teacher_name')
-    password = db.Column('password', db.String(64), doc='password')
 
-    def __init__(self, id, name, password):
-        self.id = id
-        self.teacher_name = name
+class User(db.Model):
+    __tablename__ = 'Users'
+
+    Id = db.Column('id', db.String(64), primary_key=True, doc='id')
+    UserId = db.Column('id', db.String(64), primary_key=True, doc='user_id')
+    UserName = db.Column('user_name', db.String(64), doc='user_name')
+    UserType = db.Column('user_type', Enum(TagEnum), doc='user_type')
+    IsLogin = db.Column('is_login', db.Boolean, doc='is_login')
+    Password = db.Column('password', db.String(64), doc='password')
+
+    def __init__(self, userId, userName, userType, password):
+        self.Id = str(uuid.uuid4())
+        self.UserId = userId
+        self.UserName = userName
+        self.UserType = userType
         self.password = password
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, Response, make_response, jsonify
-from .models import Teacher
+from .models import User
 import json
 from . import db
 from . import responseCode
@@ -9,46 +9,63 @@ views = Blueprint('views', __name__)
 
 @views.route('/user/login', methods=['POST', 'OPTIONS'])
 def login():
-    data = request.get_data()
-    print(data)
-    data = json.loads(data)
-    print(data)
-    userId = data['userId']
-    password = data['password']
-    userType = data['userType']
-    answer = Teacher.query.filter_by(id=userId, password=password).first()
-    if answer is None:
+    try:
+        data = request.get_data()
+        print(data)
+        data = json.loads(data)
+        print(data)
+        userId = data['userId']
+        password = data['password']
+        userType = data['userType']
+        answer = User.query.filter_by(UserId=userId, password=password, UserType=userType).first()
+        if answer is None:
+            return jsonify(
+                code=responseCode.FAIL,
+                message="Record not find!",
+                data={},
+            )
+        else:
+            answer.IsLogin = True
+            db.session.commit()
+            return jsonify(
+                code=responseCode.SUCCESS,
+                message="",
+                data={"userId": answer.id,
+                      "username": answer.teacher_name,
+                      "userType": userType,
+                      "loggedIn": answer.IsLogin},
+            )
+    except Exception as e:
+        print(e)
         return jsonify(
-            code=responseCode.FAIL,
-            message="Record not find!",
+            code=responseCode.INTERNAL_SERVER_ERROR,
+            message="INTERNAL_SERVER_ERROR!",
             data={},
-        )
-    else:
-        print(answer)
-        return jsonify(
-            code=responseCode.SUCCESS,
-            message="",
-            data={"userId": answer.id,
-                  "username": answer.teacher_name,
-                  "userType": 2,
-                  "loggedIn": True},
         )
 
 
 @views.route('/user/register', methods=['POST'])
 def register():
-    data = request.get_data()
-    print(data)
-    data = json.loads(data)
-    userId = data['userId']
-    username = data['username']
-    password = data['password']
-    userType = data['userType']
-    record = Teacher(userId, username, password)
-    db.session.add(record)
-    db.session.commit()
-    return jsonify(
-        code=responseCode.SUCCESS,
-        message="",
-        data={},
-    )
+    try:
+        data = request.get_data()
+        print(data)
+        data = json.loads(data)
+        userId = data['userId']
+        userName = data['username']
+        password = data['password']
+        userType = data['userType']
+        record = User(userId, userName, userType, password)
+        db.session.add(record)
+        db.session.commit()
+        return jsonify(
+            code=responseCode.SUCCESS,
+            message="",
+            data={},
+        )
+    except Exception as e:
+        print(e)
+        return jsonify(
+            code=responseCode.INTERNAL_SERVER_ERROR,
+            message="INTERNAL_SERVER_ERROR!",
+            data={},
+        )
